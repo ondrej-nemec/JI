91e98ad9882fefac7e1f6f3250d9aa33e9d88998
Ondřej Němec <nikdynikde@gmail.com>
2018-10-17 20:39:57 +0200

    filling os attribute

--- src/main/java/common/Env.java
+++ src/main/java/common/Env.java
@@ -1,5 +1,7 @@
 package common;
 
+import java.io.File;
+
 import common.envenums.AppMode;
 import common.envenums.SupportedOs;
 
@@ -19,11 +21,21 @@ public class Env {
 	
 	public Env() {
 		//TODO from file
-		this.os = SupportedOs.WINDOWS;		
+		this.os = getOs();		
 		this.mode = AppMode.PROD;		
 		this.databaseUrlConnection = "";		
 		this.databaseLogin = "";
 		this.databasePassword = "";		
 		this.pathToLogs = "logs";
+	}
+	
+	protected SupportedOs getOs() {
+		switch(File.separator) {
+			case "/":
+				return SupportedOs.LINUX;
+			case "\\":
+			default:
+				return SupportedOs.WINDOWS;
+		}
 	}
 }
