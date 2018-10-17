da09783424c3e3174fe9ed9ab85fe084e04a390e
Ondřej Němec <nikdynikde@gmail.com>
2018-10-17 21:28:25 +0200

    migrations are independent on env

--- src/main/java/tools/Migrate.java
+++ src/main/java/tools/Migrate.java
@@ -2,8 +2,6 @@ package tools;
 
 import org.flywaydb.core.Flyway;
 
-import common.Env;
-
 public class Migrate {
 
 	public Migrate(String url, String login, String password) {
@@ -14,8 +12,6 @@ public class Migrate {
 	}
 	
 	public static void main(String[] args) {
-		//TODO static provider of env
-		Env env = new Env();
-		new Migrate(env.databaseUrlConnection, env.databaseLogin, env.databasePassword);
+		new Migrate(args[0], args[1], args[2]);
 	}
 }
