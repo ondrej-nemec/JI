076fe66d84583a80322af889db5d94f7c6361133
Ondřej Němec <nikdynikde@gmail.com>
2018-12-25 00:15:23 +0100

    addec cond for linux, check version

--- src/main/java/tools/RunnerGenerate.java
+++ src/main/java/tools/RunnerGenerate.java
@@ -13,14 +13,19 @@ public class RunnerGenerate {
 			creator.write(
 				br,
 				"#!/bin/bash" +
-				Os.WINDOWS_NEW_LINE +
-				"# Linux" +
-				Os.LINUX_NEW_LINE +
-				"java -Dfile.encoding=UTF8 -jar " + jarName +
 				Os.WINDOWS_NEW_LINE + Os.LINUX_NEW_LINE +
-				"# Windows" +
-				Os.WINDOWS_NEW_LINE +
-				"start javaw -Dfile.encoding=UTF8 -jar " + jarName
+				"java -version" +
+				"# Linux" + Os.LINUX_NEW_LINE +
+				"if [ $? -eq 0 ]; then" + Os.LINUX_NEW_LINE +
+				"	java -Dfile.encoding=UTF8 -jar " + jarName +
+				"else" + Os.LINUX_NEW_LINE +
+				"	echo You must install java first" + Os.LINUX_NEW_LINE +
+				"fi" +
+				
+				Os.WINDOWS_NEW_LINE + Os.LINUX_NEW_LINE +
+				
+				"# Windows" + Os.WINDOWS_NEW_LINE +
+				"if errorlevel 0 start javaw -Dfile.encoding=UTF8 -jar " + jarName + " else echo You must install java first"
 			);
 		} catch (IOException e) {
 			e.printStackTrace();
