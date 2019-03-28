8b21551366a6bc9c3d6c31c3d17e9887364448ef
Ondřej Němec <nikdynikde@gmail.com>
2019-03-28 13:18:13 +0100

    path to migration

--- src/main/java/database/Migrate.java
+++ src/main/java/database/Migrate.java
@@ -5,7 +5,7 @@ import org.flywaydb.core.Flyway;
 public class Migrate {
 
 	public Migrate(final String url, final String login, final String password, final String migrationsLocation) {
-		Flyway f = Flyway.configure().dataSource(url, login, password).load();		
+		Flyway f = Flyway.configure().dataSource(url, login, password).locations(migrationsLocation).load();		
 		f.migrate();	
 	}
 	
