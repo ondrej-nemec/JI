20a7ca354fb8c0dfce559ba1415358f84479b4d6
Ondřej Němec <nikdynikde@gmail.com>
2019-04-06 22:19:54 +0200

    added log4jlogger

--- src/main/java/logging/Logger.java
+++ src/main/java/logging/Logger.java
@@ -1,7 +1,7 @@
 package logging;
 
 import common.Env;
-import logging.loggers.ConsoleLogger;
+import logging.loggers.Log4JLogger;
 import logging.loggers.NullLogger;
 
 public class Logger {
@@ -9,16 +9,18 @@ public class Logger {
 	private static Env env = null;
 	
 	public static ILogger getLogger(final String name) {
-		if (env == null)
-			//return new ConsoleLogger(name);
+		if (env == null) {
+			Log4JLogger.clearConfiguration();
 			return new NullLogger();
+		}
+		return new Log4JLogger(name, env.mode, env.getProperty("log.pathToLogs"));
 		//TODO switch by data from env
-		switch(env.mode) {
+		/*switch(env.mode) {
 			case PROD:
 			case DEV: return new ConsoleLogger(name);
 			case TEST:
 			default: return new NullLogger();
-		}
+		}*/
 	}
 	
 	public static ILogger getLogger(@SuppressWarnings("rawtypes") final Class clazz) {
