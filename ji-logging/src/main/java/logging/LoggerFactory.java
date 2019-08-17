ca92d79420a33c6375069c862ab3d312c1b731e2
Ondřej Němec <nikdynikde@gmail.com>
2019-08-17 18:10:59 +0200

    logger get by name cached

--- ji-logging/src/main/java/logging/LoggerFactory.java
+++ ji-logging/src/main/java/logging/LoggerFactory.java
@@ -1,5 +1,8 @@
 package logging;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import common.Logger;
 import logging.loggers.ConsoleLogger;
 import logging.loggers.Log4JLogger;
@@ -8,14 +11,30 @@ import logging.loggers.NullLogger;
 import utils.env.LoggerConfig;
 
 public class LoggerFactory {
-	
+
 	private final LoggerConfig config;
-	
+
+	private final Map<String, Logger> loggers;
+
 	public LoggerFactory(final LoggerConfig config) {
 		this.config = config;
+		this.loggers = new HashMap<>();
 	}
-	
-	public Logger getLogger(final String name) {		
+
+	public Logger getLogger(final String name) {
+		Logger l = loggers.get(name);
+		if (l == null) {
+			l = selectLogger(name);
+			loggers.put(name, l);
+		}
+		return l;
+	}
+
+	public Logger getLogger(final Class<?> clazz) {
+		return getLogger(clazz.getName());
+	}
+
+	private Logger selectLogger(String name) {
 		switch (config.getLoggerType().toLowerCase()) {
 		case "null": return new NullLogger();
 		case "console": return new ConsoleLogger(name);
@@ -27,10 +46,6 @@ public class LoggerFactory {
 				+ ". Supported (ci): 'null', 'console', 'native', 'log4j'"
 			);
 		}
-	}
-	
-	public Logger getLogger(@SuppressWarnings("rawtypes") final Class clazz) {
-		return getLogger(clazz.getName());
 	}
 	
 }
