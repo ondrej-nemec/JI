10148aedefc57892d3c522a97b28ce52122fceb8
Ondřej Němec <nikdynikde@gmail.com>
2019-09-21 11:19:04 +0200

    added console end to end test

---
+++ ji-common/src/test/java/common/ConsoleEndToEndTest.java
@@ -0,0 +1,18 @@
+package common;
+
+public class ConsoleEndToEndTest {
+
+	public static void main(String[] args) {
+		Console c = new Console();
+		c.out("Text printed by console");
+		c.out();
+		c.out("Text after new line");
+		
+		Object[] array = new String[] {"Printed", "array"};
+		c.out(array);
+		
+		c.out("Write some text:");
+		String a = c.in();
+		c.out("Your input is: " + a);
+	}
+}
