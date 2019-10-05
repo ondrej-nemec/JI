ea25a106b4a804eba30fb014d7edfae08225574f
Ondřej Němec <nikdynikde@gmail.com>
2019-10-05 17:35:36 +0200

    added throwing bi consumer

---
+++ ji-common/src/main/java/common/structures/ThrowingBiConsumer.java
@@ -0,0 +1,7 @@
+package common.structures;
+
+public interface ThrowingBiConsumer<T, S, E extends Throwable> {
+
+	void accept(T first, S second) throws E;
+	
+}
--- ji-common/src/main/java/common/structures/ThrowingConsumer.java
+++ ji-common/src/main/java/common/structures/ThrowingConsumer.java
@@ -2,6 +2,6 @@ package common.structures;
 
 public interface ThrowingConsumer<T, E extends Throwable> {
 
-	void accept(T connection) throws E;
+	void accept(T param) throws E;
 	
 }
