387983e370b9cdce79adae5b726eabfe9fabd780
Ondřej Němec <nikdynikde@gmail.com>
2019-10-25 23:54:05 +0200

    added database submodules

---
+++ ji-database/src/main/java/database/migration/Migration.java
@@ -0,0 +1,11 @@
+package database.migration;
+
+import querybuilder.QueryBuilder;
+
+public interface Migration {
+	
+	void migrate(QueryBuilder builder);
+	
+	void revert(QueryBuilder builder);
+
+}
---
+++ ji-database/src/main/java/database/migration/MigrationTool.java
@@ -0,0 +1,23 @@
+package database.migration;
+
+public class MigrationTool {
+	
+	
+	/*
+	 * 1 - nahrat jmena souboru do mapy - klic je jmeno, hodnota zvoleny "migrator" -> sql/class
+	 *   - !! java soubory nepridavat -> ? prvni kompilace
+	 *   - !! sesortovat podle id
+	 *   - otestovat, zda muze byt pouzito jako id
+	 *   - viz loadResources
+	 * 2 - kontrola existence záznamù migrace (tabulka), pøípadnì její vytvoøení
+	 *   - nejspíš select nad ní, a vrácení listu záznamu, pokud se bude vytváøet, tak prázdný seznam
+	 * 3 - projiti seznamu souboru, pokud jmeno neni v seznamu zaznamu, zkontrolovat, ze neni dalsi zaznam a provest
+	 *   - zapsat soubor do zaznamu migrace
+	 *   - pro provadeni migrace a zapsani do zaznamu migraci pouzit transakci
+	 * 
+	 *  v sql souborech bude moznost dat oddelovac - pro foward a revert cast
+	 * 
+	 * 
+	 */
+
+}
