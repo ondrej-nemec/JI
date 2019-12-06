87cc27debaf1d791e6fcf65f763e8e20adad45bb
Ondřej Němec <nikdynikde@gmail.com>
2019-12-06 20:22:48 +0100

    code style

--- ji-database/src/main/java/querybuilder/mysql/MySqlSelectBuilder.java
+++ ji-database/src/main/java/querybuilder/mysql/MySqlSelectBuilder.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 import database.support.DatabaseRow;
