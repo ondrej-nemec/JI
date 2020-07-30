3c42b0acb90e6d9a8c73cf8d04b781b09abb1f7a
Ondřej Němec <nikdynikde@gmail.com>
2020-07-30 15:26:23 +0200

    more escapes

--- ji-communication/src/main/java/socketCommunication/http/UrlEscape.java
+++ ji-communication/src/main/java/socketCommunication/http/UrlEscape.java
@@ -7,6 +7,7 @@ import common.structures.TwinList;
 public class UrlEscape {
 
 	private final static TwinList<String, String> ESCAPE = 
+			// TODO https://www.w3schools.com/tags/ref_urlencode.ASP
 			TwinList.fromArray(
 					t("%25", "%"),
 					t("%3F", "\\?"),
@@ -17,11 +18,22 @@ public class UrlEscape {
 					// t("\\+", " "), // cannot be here, must be escaped separatelly // escaped +
 					t("%3D", "="),
 					t("%26", "&"),
-					t("%27", "'")/*,
+					t("%27", "'"),
+					t("%7B", "{"),
+					t("%7D", "}"),
+					t("%5B", "["),
+					t("%5D", "]"),
+					t("%3C", "<"),
+					t("%3E", ">"),
+					t("%22", "\"")
+					
+					/*,
 					t("", "\""), // not escaped
 					t("", "*")// not escaped */
 			);
 	
+	// http://localhost:10123/aaa/bbb?hidden1=hiden_value&text1=%7Ba%7Dbcde&submit1=Send
+		
 	public static String unEscapeText(String text) {
 		text = text.replaceAll("\\+", " ");
 		for (String key : ESCAPE.getFirsts()) {
