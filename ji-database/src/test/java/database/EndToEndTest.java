6ed73fae80c937520de244ff48873feacaed7810
Ondřej Němec <nikdynikde@gmail.com>
2020-08-03 18:01:51 +0200

    committed not required imports

--- ji-database/src/test/java/database/EndToEndTest.java
+++ ji-database/src/test/java/database/EndToEndTest.java
@@ -12,12 +12,12 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
-
+/*
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized;*/
 import org.junit.runners.Parameterized.Parameters;
 
 import common.Logger;
