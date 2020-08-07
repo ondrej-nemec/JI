9a1b9275d4bbe48e3297ed443313b0d573cc6567
Ondřej Němec <nikdynikde@gmail.com>
2020-08-07 03:37:54 +0200

    repaired escape

--- ji-communication/src/test/java/socketCommunication/http/UrlEscapeTest.java
+++ ji-communication/src/test/java/socketCommunication/http/UrlEscapeTest.java
@@ -8,8 +8,8 @@ import socketCommunication.http.UrlEscape;
 
 public class UrlEscapeTest {
 	
-	private final String unEscapedString = "a?b/c\\\\d:e f=g&h%i+j\"k*l'm";
-	private final String escapedString = "a%3Fb%2Fc%5C%5Cd%3Ae+f%3Dg%26h%25i%2Bj\"k*l%27m";
+	private final String unEscapedString = "a?b/c\\\\d:e f=g&h%i+j\"k*l'm{n}o[p]q<r>s";
+	private final String escapedString = "a%3Fb%2Fc%5C%5Cd%3Ae+f%3Dg%26h%25i%2Bj%22k*l%27m%7Bn%7Do%5Bp%5Dq%3Cr%3Es";
 	
 	@Test
 	public void testUnEscapeTextReplacePercentages() {
@@ -21,4 +21,3 @@ public class UrlEscapeTest {
 		assertEquals(escapedString, UrlEscape.escapeText(unEscapedString));
 	}
 }
-		
