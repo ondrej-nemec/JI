3141088fe395f669832a5911535d23509913086a
Ondřej Němec <nikdynikde@gmail.com>
2020-09-09 23:08:48 +0200

    wip json object mapping

---
+++ ji-files/src/test/java/json/OutputJsonWritter.java
@@ -0,0 +1,61 @@
+package json;
+
+import java.util.List;
+import java.util.Map;
+
+public class OutputJsonWritter {
+
+	public void write(OutputJsonStream stream, Map<String, Object> json) throws JsonStreamException {
+		stream.startDocument();
+		for (String key : json.keySet()) {
+			writeObject(stream, json.get(key), key);
+		}
+		stream.endDocument();
+	}
+	
+	private void write(OutputJsonStream stream, Map<String, Object> objects, String name) throws JsonStreamException {
+		if (name == null) {
+			stream.writeObjectStart();
+		} else {
+			stream.writeObjectStart(name);
+		}
+		for (String key : objects.keySet()) {
+			writeObject(stream, objects.get(key), key);
+		}
+		stream.writeObjectEnd();
+	}
+	
+	private void write(OutputJsonStream stream, List<Object> list, String name) throws JsonStreamException {
+		if (name == null) {
+			stream.writeListStart();
+		} else {
+			stream.writeListStart(name);
+		}
+		for (Object o : list) {
+			writeObject(stream, o, null);
+		}
+		stream.writeListEnd();
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	private void writeObject(OutputJsonStream stream, Object value, String name) throws JsonStreamException {
+		if (value == null) {
+			if (name == null) {
+				stream.writeListValue(value);
+			} else {
+				stream.writeObjectValue(name, value);
+			}
+		} else if (value instanceof List) {
+			write(stream, (List<Object>)value, name);
+		} else if (value instanceof Map) {
+			write(stream, (Map<String, Object>)value, name);
+		} else {
+			if (name == null) {
+				stream.writeListValue(value);
+			} else {
+				stream.writeObjectValue(name, value);
+			}
+		}	
+	}
+}
