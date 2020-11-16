99b73609c8d87334a167eb373b52ee8294edc357
Ondřej Němec <nikdynikde@gmail.com>
2020-11-16 22:34:56 +0100

    code style

--- ji-acl/src/main/java/helper/AuthorizationHelper.java
+++ ji-acl/src/main/java/helper/AuthorizationHelper.java
@@ -32,35 +32,38 @@ public class AuthorizationHelper {
 		Rules rules = rulesDao.getRulesForUser(who, where);
 		
 		Action userId = rules.getForUserId();		
-		if (userId != Action.UNDEFINED)
+		if (userId != Action.UNDEFINED) {
 			return isAllowed(userId, what);
-				
+		}
+		
 		Action userRank = rules.getForUserRank();
-		if (userRank != Action.UNDEFINED)
-			return isAllowed(userRank, what);		
+		if (userRank != Action.UNDEFINED) {
+			return isAllowed(userRank, what);
+		}
 		
 		Action roleId = Action.UNDEFINED;
 		for (Action actualRole : rules.getForRoleIds()) {
 			roleId = selectRole(roleId, actualRole);
 		}
-		if (roleId != Action.UNDEFINED)
-			return isAllowed(roleId, what);		
+		if (roleId != Action.UNDEFINED) {
+			return isAllowed(roleId, what);
+		}
 		
 		logger.warn("No access rule for: " + who + " -> " + where + " -> " + what); 
 		return false;
 	}
 
 	private Action selectRole(Action origin, Action actual) {
-		if (origin.ordinal() >= actual.ordinal())
+		if (origin.ordinal() >= actual.ordinal()) {
 			return origin;
+		}
 		return actual;
 	}
 	
 	private boolean isAllowed(Action finded, Action required) {
-		if (finded == Action.FORBIDDEN)
-			return false;
-		if (finded.ordinal() >= required.ordinal())
-			return true;
+		if (finded != Action.FORBIDDEN) {
+			return finded.ordinal() >= required.ordinal();
+		}
 		return false;
 	}
 	
