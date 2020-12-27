3bbda7952230099c82a83294499e2936b9128c4d
Ondřej Němec <nikdynikde@gmail.com>
2020-12-28 00:01:13 +0100

    wip derby datetime

--- ji-database/src/main/java/querybuilder/derby/EnumToDerbyString.java
+++ ji-database/src/main/java/querybuilder/derby/EnumToDerbyString.java
@@ -51,6 +51,7 @@ public class EnumToDerbyString {
 		switch (type.getType()) {
     		case STRING:
     			return String.format("VARCHAR(%s)", type.getSize());
+    		case DATETIME: return "TIMESTAMP";
     		default: return type.getType().toString();
 		}
 	}
