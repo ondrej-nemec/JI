9a5687a8d4c1ede43e60f3161dbe388083ebbbfd
Ondřej Němec <nikdynikde@gmail.com>
2021-01-07 15:44:12 +0100

    added files list

---
+++ ji-files/src/test/java/core/FilesListTest.java
@@ -0,0 +1,34 @@
+package core;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class FilesListTest {
+
+	@Test
+	public void testGetFolderContentFromClasspathDir() throws Exception {
+		List<String> folders = Arrays.asList(
+			"app.properties",
+			"Empty.class",
+			"subDir/env.properties"
+		);
+		assertEquals(folders, FilesList.get("core/filesList", true).getFiles());
+	}
+
+	@Test
+	public void testGetFolderContentFromDir() throws Exception {
+		List<String> folders = Arrays.asList(
+			"app.properties",
+			"Empty.java",
+			"subDir/env.properties"
+		);
+		assertEquals(folders, FilesList.get("tests/core/filesList", true).getFiles());
+	}
+	
+	// TODO add test for jar
+	
+}
---
+++ ji-files/tests/core/filesList/Empty.java
@@ -0,0 +1,5 @@
+package code.filesList;
+
+public class Empty {
+
+}
