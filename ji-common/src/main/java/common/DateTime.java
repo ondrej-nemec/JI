352d032c67fa1a64af66245a060fac4c20ac8739
Ondřej Němec <nikdynikde@gmail.com>
2021-01-11 19:09:08 +0100

    datetime added time from calendar

--- ji-common/src/main/java/common/DateTime.java
+++ ji-common/src/main/java/common/DateTime.java
@@ -2,6 +2,7 @@ package common;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 
 public class DateTime {
 	
@@ -13,5 +14,20 @@ public class DateTime {
 		DateTimeFormatter format = DateTimeFormatter.ofPattern(pattern);
 		return date.format(format);
 	}
+	
+	public static String format(String pattern, Calendar cal) {
+		return DateTime.format("yyyy-MM-dd H:m:s", LocalDateTime.of(
+                 cal.get(Calendar.YEAR),
+                 cal.get(Calendar.MONTH)+1,
+                 cal.get(Calendar.DAY_OF_MONTH),
+                 cal.get(Calendar.HOUR_OF_DAY),
+                 cal.get(Calendar.MINUTE),
+                 cal.get(Calendar.SECOND)
+        ));
+	}
 
+	public static String format(String pattern, int year, int month, int day, int hour, int minutes, int second) {
+		return DateTime.format("yyyy-MM-dd H:m:s", LocalDateTime.of(year, month, day, hour, minutes, second));
+	}
+	
 }
