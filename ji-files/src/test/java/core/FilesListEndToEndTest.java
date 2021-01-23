7bb46f360eb710f7654400302e982f303738d0f8
Ondřej Němec <nikdynikde@gmail.com>
2021-01-23 01:02:13 +0100

    wip - files list test

---
+++ ji-files/src/test/java/core/FilesListEndToEndTest.java
@@ -0,0 +1,21 @@
+package core;
+
+public class FilesListEndToEndTest {
+
+	public static void main(String[] args) {
+		for (String folder : new String[] {"tests", "json"}) {
+			try {
+				System.out.println("-- Folder: " + folder);
+				FilesList l = FilesList.get(folder, true);
+				System.out.println(l.getURL());
+				l.getFiles().forEach((file)->{
+					System.out.println("* " + file);
+				});
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+}
