a7e012df53867cab5bf870ff84df78afb4c98417
ondrej-nemec <42703271+ondrej-nemec@users.noreply.github.com>
2021-02-15 12:48:31 +0100

    Json write Jsonable fix

--- ji-files/src/main/java/json/OutputJsonWritter.java
+++ ji-files/src/main/java/json/OutputJsonWritter.java
@@ -56,7 +56,7 @@ public class OutputJsonWritter {
 				stream.writeObjectValue(name, value);
 			}
 		} else if (value instanceof Jsonable) {
-			writeObject(stream, ((Jsonable)value).toJson(), name);
+			write(stream, ((Jsonable)value).toJson(), name);
 		} else if (value instanceof List) {
 			write(stream, (List<Object>)value, name);
 		} else if (value instanceof Map) {
