cc1f0d2c07366bb446971804f1ada44053aec9b6
Ondřej Němec <nikdynikde@gmail.com>
2021-02-18 22:04:17 +0100

    request parameters use dictionary

--- ji-communication/src/main/java/socketCommunication/http/server/RequestParameters.java
+++ ji-communication/src/main/java/socketCommunication/http/server/RequestParameters.java
@@ -5,8 +5,9 @@ import java.util.List;
 import java.util.Map;
 
 import common.structures.Tuple2;
+import utils.Dictionary;
 
-public class RequestParameters extends HashMap<String, Object> {
+public class RequestParameters extends HashMap<String, Object> implements Dictionary {
 
 	private static final long serialVersionUID = 1L;
 	
@@ -17,25 +18,9 @@ public class RequestParameters extends HashMap<String, Object> {
 		}
 	}
 	
-	public String getString(String key) {
-		Object val = get(key);
-		return val == null ? null : val.toString();
-	}
-	
-	public Boolean getBoolean(String key) {
-		return Boolean.parseBoolean(getString(key));
-	}
-
-	public Integer getInt(String key) {
-		return Integer.parseInt(getString(key));
-	}
-	
-	public Long getLong(String key) {
-		return Long.parseLong(getString(key));
-	}
-	
-	public Double getDouble(String key) {
-		return Double.parseDouble(getString(key));
+	@Override
+	public Object getValue(String name) {
+		return get(name);
 	}
 	
 	public List<?> getList(String key) {
@@ -45,47 +30,7 @@ public class RequestParameters extends HashMap<String, Object> {
 	public Map<?, ?> getMap(String key) {
 		return Map.class.cast(get(key));
 	}
-	
-	/*
-	@Override
-	public String toString() {
-		StringBuilder b = new StringBuilder();
-		forEach((key, item)->{
-			
-			b.append(String.format("{%s=%s[%s]},", key, item.getClass(), item));
-		});
-		return mapToString(this, "RequestParameters:");
-	}
-	
-	private String mapToString(Map<String, ?> map, String name) {
-		StringBuilder b = new StringBuilder(name + "{");
-		map.forEach((key, item)->{
-			if (item instanceof Map) {
-				b.append(mapToString((Map)item, key));
-			} else if (item instanceof List) {
-				b.append(listToString((List)item, key));
-			} else {
-				b.append(String.format("%s=%s->%s,", key, item.getClass(), item));
-			}
-		});
-		b.append("};");
-		return b.toString();
-	}
-	
-	private String listToString(List<?> list, String name) {
-		StringBuilder b = new StringBuilder(name + "[");
-		list.forEach((item)->{
-			if (item instanceof Map) {
-				b.append(mapToString((Map)item, ""));
-			} else if (item instanceof List) {
-				b.append(listToString((List)item, ""));
-			} else {
-				b.append(String.format("%s->%s,", item.getClass(), item));
-			}
-		});
-		b.append("];");
-		return b.toString();
-	}
-	
-	//*/
+
+
+
 }
