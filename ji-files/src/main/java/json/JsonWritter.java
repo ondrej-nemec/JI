452e2c9b192ae8b25ac9604e93123da4f3a5405a
Ondřej Němec <nikdynikde@gmail.com>
2021-02-21 22:12:13 +0100

    json fix - iterable instead of list

--- ji-files/src/main/java/json/JsonWritter.java
+++ ji-files/src/main/java/json/JsonWritter.java
@@ -1,6 +1,5 @@
 package json;
 
-import java.util.List;
 import java.util.Map;
 
 import json.providers.OutputStringProvider;
@@ -44,7 +43,7 @@ public class JsonWritter {
 		stream.writeObjectEnd();
 	}
 	
-	private void write(OutputJsonStream stream, List<Object> list, String name) throws JsonStreamException {
+	private void write(OutputJsonStream stream, Iterable<Object> list, String name) throws JsonStreamException {
 		if (name == null) {
 			stream.writeListStart();
 		} else {
@@ -67,8 +66,8 @@ public class JsonWritter {
 			}
 		} else if (value instanceof Jsonable) {
 			writeObject(stream, ((Jsonable)value).toJson(), name);
-		} else if (value instanceof List) {
-			write(stream, (List<Object>)value, name);
+		} else if (value instanceof Iterable) {
+			write(stream, (Iterable<Object>)value, name);
 		} else if (value instanceof Map) {
 			write(stream, (Map<String, Object>)value, name);
 		} else {
---
+++ ji-files/src/test/java/json/JsonWritterTest.java
@@ -0,0 +1,22 @@
+package json;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class JsonWritterTest {
+
+	@Test
+	public void testWriteJson() {
+		Map<String, Object> json = new HashMap<>();
+		json.put("aa", Arrays.asList("some-text"));
+		json.put("bb", "another-text");
+		JsonWritter w = new JsonWritter();
+		assertEquals("{\"aa\":[\"some-text\"],\"bb\":\"another-text\"}", w.write(json));
+	}
+	
+}
