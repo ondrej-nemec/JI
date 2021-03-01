aa5bd0b911529f912000840e04991221662604ee
Ondřej Němec <nikdynikde@gmail.com>
2021-03-01 20:54:14 +0100

    implode null pointer fix

--- ji-common/src/main/java/common/Implode.java
+++ ji-common/src/main/java/common/Implode.java
@@ -7,7 +7,7 @@ public class Implode {
 	
 	@SafeVarargs
 	public static <T> String implode(String glue, T... data) {
-		return implode((a)->a.toString(), glue, data);
+		return implode((a)->a==null?null:a.toString(), glue, data);
 	}
 
 	@SafeVarargs
@@ -16,7 +16,7 @@ public class Implode {
 	}
 
 	public static <T> String implode(String glue, Iterable<T> data) {
-		return implode((a)->a.toString(), glue, data);
+		return implode((a)->a==null?null:a.toString(), glue, data);
 	}
 
 	public static <T> String implode(Function<T, String> toString, String glue, Iterable<T> data) {
@@ -29,29 +29,4 @@ public class Implode {
 		}
 		return result.toString();
 	}
-	
-	/*
-	@Deprecated
-	public static <T> String implode(String glue, T[] data, int from, int to) {
-		return implode(glue, data, from, to, (a)->a.toString());
-	}
-
-	@Deprecated
-	public static <T> String implode(String glue, T[] data, int from, int to, Function<T, String> toString) {
-		if (to >= data.length || from < 0 || from > to) {
-			throw new LogicException(String.format(
-					"Incorrect values of from (%d), to (%d) in relation with length of array (%d)",
-					from, to, data.length
-				));
-		}
-		StringBuilder result = new StringBuilder();
-		for (int i = from; i <= to; i++) {
-			if (i != from) {
-				result.append(glue);
-			}
-			result.append(toString.apply(data[i]));
-		}
-		return result.toString();
-	}
-	*/
 }
