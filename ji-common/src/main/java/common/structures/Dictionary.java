f7877d7417259b387d6da74ac4fbeb4f376c4324
Ondřej Němec <nikdynikde@gmail.com>
2021-05-27 23:22:36 +0200

    dictionary value parsing generally

--- ji-common/src/main/java/common/structures/Dictionary.java
+++ ji-common/src/main/java/common/structures/Dictionary.java
@@ -1,113 +1,74 @@
 package common.structures;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 
 public interface Dictionary<S> {
 
 	Object getValue(S name);
+	
+	default DictionaryValue getDictionaryValue(S name) {
+		return new DictionaryValue(getValue(name));
+	}
 
 	default Object get(S name) {
 		return getValue(name);
 	}
 	
-	default <T> T getValue(S name, Class<T> clazz, Function<Object, Object> prepare) {
-		Object value = getValue(name);
-		if (value == null) {
-			return null;
-		}
-		value = prepare.apply(value);
-		return clazz.cast(value);
-	}
-	
-	default <T> T getValue(S name, Function<String, T> create) {
-		Object value = getValue(name);
-		if (value == null) {
-			return null;
-		}
-	// TODO merge this two getValue to one 
-		// if (value instanceof String) {}
-		return create.apply(value.toString());
-	}
-	
 	default Boolean getBoolean(S name) {
-		return getValue(name, a->Boolean.parseBoolean(a));
+		return getDictionaryValue(name).getBoolean();
+	}
+	
+	default Byte getByte(S name) {
+		return getDictionaryValue(name).getByte();
 	}
 	
 	default Integer getInteger(S name) {
-		return getValue(name, a->Integer.parseInt(a));
-	}
-	
-	default Double getDouble(S name) {
-		return getValue(name, a->Double.parseDouble(a));
+		return getDictionaryValue(name).getInteger();
 	}
 	
 	default Long getLong(S name) {
-		return getValue(name, a->Long.parseLong(a));
+		return getDictionaryValue(name).getLong();
+	}
+	
+	default Float getFloat(S name) {
+		return getDictionaryValue(name).getFloat();
+	}
+	
+	default Double getDouble(S name) {
+		return getDictionaryValue(name).getDouble();
+	}
+	
+	default Character getCharacter(S name) {
+		return getDictionaryValue(name).getCharacter();
 	}
 	
 	default String getString(S name) {
-		return getValue(name, a->a);
+		return getDictionaryValue(name).getString();
 	}
 	
 	default <E extends Enum<E>> E getEnum(S name, Class<E> enumm) {
-		return getValue(name, a->E.valueOf(enumm, a));
+		return getDictionaryValue(name).getEnum(enumm);
 	}
 	
 	default List<String> getList(S name, String delimiter) {
-		return getValue(name, a->Arrays.asList(a.split(delimiter)));
-	}
-/*
-	@SuppressWarnings("unchecked")
-	default <T> ListDictionary<T> getAsDictionaryList(S name) {
-		return new ListDictionary<T>(getValue(name, List.class));
+		return getDictionaryValue(name).getList(delimiter);
 	}
 
-	@SuppressWarnings("unchecked")
-	default <T, E> MapDictionary<T, E> getAsDictionaryMap(S name) {
-		return new MapDictionary<T, E>(getValue(name, Map.class));
-	}
-*/
-	@SuppressWarnings("unchecked")
 	default <T> ListDictionary<T> getDictionaryList(S name) {
-		return getValue(name, ListDictionary.class, (value)->{
-			if (value instanceof List<?>) {
-				return new ListDictionary<T>(List.class.cast(value));
-			}
-			return value;
-		});
+		return getDictionaryValue(name).getDictionaryList();
 	}
 
-	@SuppressWarnings("unchecked")
 	default <T, E> MapDictionary<T, E> getDictionaryMap(S name) {
-		return getValue(name, MapDictionary.class, (value)->{
-			if (value instanceof Map<?, ?>) {
-				return new MapDictionary<T, E>(Map.class.cast(value));
-			}
-			return value;
-		});
+		return getDictionaryValue(name).getDictionaryMap();
 	}
 	
-	@SuppressWarnings("unchecked")
 	default <T> List<T> getList(S name) {
-		return getValue(name, List.class, (value)-> {
-			if (value instanceof ListDictionary<?>) {
-				return ListDictionary.class.cast(value).toList();
-			}
-			return value;
-		});
+		return getDictionaryValue(name).getList();
 	}
 
-	@SuppressWarnings("unchecked")
 	default <K, V> Map<K, V> getMap(S name) {
-		return getValue(name, Map.class, (value)-> {
-			if (value instanceof MapDictionary<?, ?>) {
-				return MapDictionary.class.cast(value).toMap();
-			}
-			return value;
-		});
+		return getDictionaryValue(name).getMap();
 	}
 
 }
--- ji-common/src/main/java/common/structures/DictionaryValue.java
+++ ji-common/src/main/java/common/structures/DictionaryValue.java
@@ -9,62 +9,123 @@ public class DictionaryValue {
 
 	private final Object value;
 	
+	private Function<String, Object> fromStringToListCallback = null;
+	private Function<String, Object> fromStringToMapCallback = null;
+	
 	public DictionaryValue(Object value) {
 		this.value = value;
 	}
+	
+	public void addListCallback(Function<String, Object> fromStringToListCallback) {
+		this.fromStringToListCallback = fromStringToListCallback;
+	}
+	
+	public void addMapCallback(Function<String, Object> fromStringToMapCallback) {
+		this.fromStringToMapCallback = fromStringToMapCallback;
+	}
+	
+	/******/
 
 	public Object getValue() {
 		return value;
 	}
-	
-	public <T> T getValue(Class<T> clazz, Function<Object, Object> prepare) {
+
+	@SuppressWarnings("unchecked")
+	public <T> T getValue(Class<T> clazz) {
 		if (value == null) {
 			return null;
 		}
-		if (clazz.isInstance(value)) {
-			return clazz.cast(value);
-		}
-		return clazz.cast(prepare.apply(value));
+		return (T)getParsedVal(clazz); // with clazz.cast cannot cast object to primitive
 	}
 	
-	public <T> T parseValue(Function<String, T> create) {
-		if (value == null) {
-			return null;
+	@SuppressWarnings("unchecked")
+	private <E extends Enum<E>, T> Object getParsedVal(Class<T> clazz) {
+		if (clazz.isAssignableFrom(Boolean.class) || clazz.isAssignableFrom(boolean.class)) {
+			return getBoolean();
+		} else if (clazz.isAssignableFrom(Byte.class) || clazz.isAssignableFrom(byte.class)) {
+			return getByte();
+		} else if (clazz.isAssignableFrom(Short.class) || clazz.isAssignableFrom(short.class)) {
+			return getShort();
+		} else if (clazz.isAssignableFrom(Integer.class) || clazz.isAssignableFrom(int.class)) {
+			return getInteger();
+		} else if (clazz.isAssignableFrom(Long.class) || clazz.isAssignableFrom(long.class)) {
+			return getLong();
+		} else if (clazz.isAssignableFrom(Float.class) || clazz.isAssignableFrom(float.class)) {
+			return getFloat();
+		} else if (clazz.isAssignableFrom(Double.class) || clazz.isAssignableFrom(double.class)) {
+			return getDouble();
+		} else if (clazz.isAssignableFrom(Character.class) || clazz.isAssignableFrom(char.class)) {
+			return getCharacter();
+		} else if (clazz.isAssignableFrom(Map.class)) {
+			return getMap();
+		} else if (clazz.isAssignableFrom(List.class)) {
+			return getList();
+		} else if (clazz.isAssignableFrom(MapDictionary.class)) {
+			return getDictionaryMap();
+		} else if (clazz.isAssignableFrom(ListDictionary.class)) {
+			return getDictionaryList();
+		} else if (clazz.isEnum()) {
+			return getEnum((Class<E>)clazz);
+		} else {
+			return value;
 		}
-		return create.apply(value.toString());
 	}
+	
+	/**************/
 	
 	public Boolean getBoolean() {
-		return parseValue(a->Boolean.parseBoolean(a));
+		return parseValue(
+			Boolean.class,
+			v->v.toString().equalsIgnoreCase("true") || v.toString().equalsIgnoreCase("on") || v.toString().equalsIgnoreCase("1")
+		);
+	}
+	
+	public Byte getByte() {
+		return parseValue(Byte.class, a->Byte.parseByte(a), v->Number.class.cast(v).byteValue());
+	}
+	
+	public Short getShort() {
+		return parseValue(Short.class, a->Short.parseShort(a), v->Number.class.cast(v).shortValue());
 	}
 	
 	public Integer getInteger() {
-		return parseValue(a->Integer.parseInt(a));
-	}
-	
-	public Double getDouble() {
-		return parseValue(a->Double.parseDouble(a));
+		return parseValue(Integer.class, a->Integer.parseInt(a), v->Number.class.cast(v).intValue());
 	}
 	
 	public Long getLong() {
-		return parseValue(a->Long.parseLong(a));
+		return parseValue(Long.class, a->Long.parseLong(a), v->Number.class.cast(v).longValue());
+	}
+	
+	public Float getFloat() {
+		return parseValue(Float.class, a->Float.parseFloat(a), v->Number.class.cast(v).floatValue());
+	}
+	
+	public Double getDouble() {
+		return parseValue(Double.class, a->Double.parseDouble(a), v->Number.class.cast(v).doubleValue());
+	}
+	
+	public Character getCharacter() {
+		return parseValue(Character.class, a->a.charAt(0));
 	}
 	
 	public String getString() {
-		return parseValue(a->a);
+		return parseValue(String.class, a->a);
 	}
 	
 	public <E extends Enum<E>> E getEnum(Class<E> enumm) {
-		return parseValue(a->E.valueOf(enumm, a));
-	}
-	
-	public List<String> getList(String delimiter) {
-		return parseValue(a->Arrays.asList(a.split(delimiter)));
+		return parseValue(enumm, a->E.valueOf(enumm, a));
 	}
 
 	@SuppressWarnings("unchecked")
+	public List<String> getList(String delimiter) {
+		return parseValue(List.class, a->Arrays.asList(a.split(delimiter)));
+	}
+	
+	/************/
+
+	@SuppressWarnings("unchecked")
 	public <T> ListDictionary<T> getDictionaryList() {
-		return getValue(ListDictionary.class, (value)->{
+		return parseValue(ListDictionary.class, fromStringToListCallback, (value)->{
 			if (value instanceof List<?>) {
 				return new ListDictionary<T>(List.class.cast(value));
 			}
@@ -74,7 +135,7 @@ public class DictionaryValue {
 
 	@SuppressWarnings("unchecked")
 	public <T, E> MapDictionary<T, E> getDictionaryMap() {
-		return getValue(MapDictionary.class, (value)->{
+		return parseValue(MapDictionary.class, fromStringToMapCallback, (value)->{
 			if (value instanceof Map<?, ?>) {
 				return new MapDictionary<T, E>(Map.class.cast(value));
 			}
@@ -84,7 +145,7 @@ public class DictionaryValue {
 	
 	@SuppressWarnings("unchecked")
 	public <T> List<T> getList() {
-		return getValue(List.class, (value)-> {
+		return parseValue(List.class, fromStringToListCallback, (value)-> {
 			if (value instanceof ListDictionary<?>) {
 				return ListDictionary.class.cast(value).toList();
 			}
@@ -94,7 +155,7 @@ public class DictionaryValue {
 
 	@SuppressWarnings("unchecked")
 	public <K, V> Map<K, V> getMap() {
-		return getValue(Map.class, (value)-> {
+		return parseValue(Map.class, fromStringToMapCallback, (value)-> {
 			if (value instanceof MapDictionary<?, ?>) {
 				return MapDictionary.class.cast(value).toMap();
 			}
@@ -102,6 +163,31 @@ public class DictionaryValue {
 		});
 	}
 	
+	/***********/
+
+	private <T> T parseValue(Class<T> clazz, Function<String, Object> fromString, Function<Object, Object> prepare) {
+		if (value == null) {
+			return null;
+		}
+		if (clazz.isInstance(value)) {
+			return clazz.cast(value);
+		}
+		Object val = value;
+		if (val instanceof String && fromString != null) {
+			val = fromString.apply(val.toString());
+		}
+		if (prepare != null) {
+			val = prepare.apply(val);
+		}
+		return clazz.cast(val);
+	}
+	
+	private <T> T parseValue(Class<T> clazz, Function<String, Object> create) {
+		return parseValue(clazz, create, null);
+	}
+	
+	/************/
+
 	@Override
 	public String toString() {
 		return value.toString();
