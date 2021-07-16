c2faee8bac71cac64a1f25e59b5f694ad53eeb5b
Ondřej Němec <nikdynikde@gmail.com>
2021-07-16 22:29:57 +0200

    common added hash class

---
+++ ji-common/src/main/java/common/functions/Hash.java
@@ -0,0 +1,40 @@
+package common.functions;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import common.exceptions.HashException;
+
+public class Hash {
+	
+	private final String algoritm;	
+	
+	public Hash(String algoritm) {
+		this.algoritm = algoritm;
+	}
+
+	public String toHash(String message) throws HashException {
+		try {
+			MessageDigest digest = MessageDigest.getInstance(algoritm);
+			byte[] hash = Base64.getEncoder().encode(
+				digest.digest(
+					message.getBytes(StandardCharsets.UTF_8)
+				)
+			);
+			return new String(hash);
+		} catch (NoSuchAlgorithmException e) {
+			throw new HashException(e);
+		}
+	}
+	
+	public boolean compare(String message, String hash) {
+		try {
+			return hash.equals(toHash(message));
+		} catch (HashException e) {
+			return false;
+		}
+	}
+
+}
