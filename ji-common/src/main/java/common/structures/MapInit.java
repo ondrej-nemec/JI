def5b03856a2429900157c4534a120f8f80e4b18
Ondřej Němec <nikdynikde@gmail.com>
2021-09-29 22:40:47 +0200

    map init removed deprecated methods

--- ji-common/src/main/java/common/structures/MapInit.java
+++ ji-common/src/main/java/common/structures/MapInit.java
@@ -39,48 +39,4 @@ public class MapInit<K, V> {
 	public MapDictionary<K, V> toDictionaryMap() {
 		return new MapDictionary<>(map);
 	}
-	
-	/**********************/
-
-	@Deprecated
-	public static <K, V> Tuple2<K, V> t(K key, V value) {
-		return new Tuple2<K, V>(key, value);
-	}
-
-	@SuppressWarnings("unchecked")
-	@Deprecated
-	private Map<K, V> map(Map<K, V>map, Tuple2<K, V>... kvs) {
-		for (Tuple2<K, V> tuple : kvs) {
-			map.put(tuple._1(), tuple._2());
-		}
-		return map;
-	}
-
-	@SafeVarargs
-	@Deprecated
-	public static <K, V> UniqueMap<K, V> uniqueMap(Tuple2<K, V>... kvs) {
-		UniqueMap<K, V> map = new UniqueMap<>();
-		for (Tuple2<K, V> kv : kvs) {
-			map.put(kv._1(), kv._2());
-		}
-		return map;
-	}
-	
-	@SafeVarargs
-	@Deprecated
-	public static <K, V> Map<K, V> hashMap(Tuple2<K, V>... kvs) {
-		MapInit<K, V> init = new MapInit<>();
-		return init.map(new HashMap<>(), kvs);
-	}
-
-	@SafeVarargs
-	@Deprecated
-	public static Properties properties(Tuple2<?, ?>... kvs) {
-		Properties prop = new Properties();
-		for (Tuple2<?, ?> tuple : kvs) {
-			prop.put(tuple._1(), tuple._2());
-		}
-		return prop;
-	}
-	
 }
