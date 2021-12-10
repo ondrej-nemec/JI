e076c6c3ef743e1a3e44d53d2a612fecdec0e9c2
Ondřej Němec <nikdynikde@gmail.com>
2021-12-10 22:48:46 +0100

    escape parameters moved to sql

--- ji-database/src/main/java/ji/querybuilder/builders/InsertBuilder.java
+++ ji-database/src/main/java/ji/querybuilder/builders/InsertBuilder.java
@@ -1,8 +1,6 @@
 package ji.querybuilder.builders;
 
-import java.util.Map;
-
-import ji.querybuilder.buildersparent.ParametrizedBuilder;
+import ji.querybuilder.enums.SQL;
 import ji.querybuilder.executors.InsertExecute;
 
 public interface InsertBuilder extends InsertExecute<InsertBuilder> {
@@ -10,16 +8,7 @@ public interface InsertBuilder extends InsertExecute<InsertBuilder> {
 	InsertBuilder addNotEscapedValue(String columnName, String value);
 	
 	default InsertBuilder addValue(String columnName, Object value) {
-		// TODO use some static func here and in parametrized to remove this anonymous class ??
-		new ParametrizedBuilder<InsertBuilder>() {
-			@Override public String getSql() { return null; }
-			@Override public Map<String, String> getParameters() { return null; }
-			@Override public InsertBuilder addNotEscapedParameter(String name, String val) {
-				addNotEscapedValue(name, val);
-				return null;
-			}
-		}.addParameter(columnName, value);
-		return this;
+		return addNotEscapedValue(columnName, SQL.escape(value));
 	}
 
 }
--- ji-database/src/main/java/ji/querybuilder/enums/SQL.java
+++ ji-database/src/main/java/ji/querybuilder/enums/SQL.java
@@ -1,8 +1,44 @@
 package ji.querybuilder.enums;
 
-public class SQL {
+import java.time.temporal.Temporal;
 
-	public static String escape(String sql) {
+import ji.common.functions.Implode;
+import ji.common.structures.ListDictionary;
+
+public class SQL {
+	
+	public static String escape(Object value) {
+		if (value == null) {
+			return "null";
+		}
+		if (value instanceof Iterable<?>) {
+			Iterable<?> iterable = Iterable.class.cast(value);
+			return Implode.implode(item->escapeScalar(item), ",", iterable);
+		} else if (value instanceof ListDictionary) {
+			ListDictionary<?> iterable = ListDictionary.class.cast(value);
+			return Implode.implode(item->escapeScalar(item), ",", iterable.toList());
+		} else {
+			return escapeScalar(value);
+		}
+	}
+
+	public static String escapeScalar(Object value) {
+		Class<?> clazz = value.getClass();
+		if (clazz.isAssignableFrom(Boolean.class) || clazz.isAssignableFrom(boolean.class)) {
+			//  value ? "1" : "0"
+			return value.toString();
+		} else if (value instanceof Number) {
+			return value.toString();
+		} else if (clazz.isPrimitive() && !(clazz.isAssignableFrom(byte.class) || clazz.isAssignableFrom(char.class))) {
+			return value.toString();
+		} else if (value instanceof Temporal) {
+			return escapeString(value.toString().replace("T", " "));
+		} else {
+			return escapeString(value.toString());
+		}
+	}
+
+	public static String escapeString(String sql) {
 		// TODO??  * @ - _ + . /
 		return String.format("'%s'", sql.replaceAll("\\'", "''"));
 	}
