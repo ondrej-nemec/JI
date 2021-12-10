9d1be9c4a731c74c07560c1332c2b072511a341f
Ondřej Němec <nikdynikde@gmail.com>
2021-12-10 22:49:16 +0100

    code style

--- ji-database/src/main/java/ji/querybuilder/builders/SelectBuilder.java
+++ ji-database/src/main/java/ji/querybuilder/builders/SelectBuilder.java
@@ -3,31 +3,5 @@ package ji.querybuilder.builders;
 import ji.querybuilder.executors.SelectExecute;
 
 public interface SelectBuilder extends SelectExecute<SelectBuilder>, Select<SelectBuilder> {
-/*
-	SelectBuilder from(String table);
-	
-	default SelectBuilder from(SelectBuilder builder) {
-		return from(String.format("(%s)", builder.createSql()));
-	}
-	
-	SelectBuilder join(String table, Join join, String on);
 
-	default SelectBuilder join(SelectBuilder builder, Join join, String on) {
-		return join(String.format("(%s)", builder.createSql()), join, on);
-	}
-	
-	SelectBuilder where(String where);
-	
-	SelectBuilder andWhere(String where);
-	
-	SelectBuilder orWhere(String where);
-	
-	SelectBuilder orderBy(String orderBy);
-	
-	SelectBuilder groupBy(String groupBy);
-	
-	SelectBuilder having(String having);
-	
-	SelectBuilder limit(int limit, int offset);
-	*/
 }
