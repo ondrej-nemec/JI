b8c831b543afbeef07e3e070f1a94b4e36ce6572
Ondřej Němec <nikdynikde@gmail.com>
2022-02-07 20:16:06 +0100

    datetime refactored and remove deprecation

--- ji-common/src/main/java/ji/common/functions/DateTime.java
+++ ji-common/src/main/java/ji/common/functions/DateTime.java
@@ -2,34 +2,17 @@ package ji.common.functions;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
+import java.time.temporal.TemporalAccessor;
 
-@Deprecated
-// TODO move to Dictionary value - getString
 public class DateTime {
 	
 	public static String format(String pattern) {
 		return format(pattern, LocalDateTime.now());
 	}
 	
-	public static String format(String pattern, LocalDateTime date) {
+	public static String format(String pattern, TemporalAccessor date) {
 		DateTimeFormatter format = DateTimeFormatter.ofPattern(pattern);
-		return date.format(format);
-	}
-	
-	public static String format(String pattern, Calendar cal) {
-		return DateTime.format("yyyy-MM-dd H:m:s", LocalDateTime.of(
-                 cal.get(Calendar.YEAR),
-                 cal.get(Calendar.MONTH)+1,
-                 cal.get(Calendar.DAY_OF_MONTH),
-                 cal.get(Calendar.HOUR_OF_DAY),
-                 cal.get(Calendar.MINUTE),
-                 cal.get(Calendar.SECOND)
-        ));
-	}
-
-	public static String format(String pattern, int year, int month, int day, int hour, int minutes, int second) {
-		return DateTime.format("yyyy-MM-dd H:m:s", LocalDateTime.of(year, month, day, hour, minutes, second));
+		return format.format(date);// date.format(format);
 	}
 	
 }
