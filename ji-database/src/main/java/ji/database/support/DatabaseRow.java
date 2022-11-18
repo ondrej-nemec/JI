7baa9980546a72d56a0cab15c75d04125dfbe23c
Ondřej Němec <nikdynikde@gmail.com>
2022-11-18 21:08:09 +0100

    db row override puts

--- ji-database/src/main/java/ji/database/support/DatabaseRow.java
+++ ji-database/src/main/java/ji/database/support/DatabaseRow.java
@@ -16,6 +16,18 @@ public class DatabaseRow extends MapDictionary<String, Object> {
 		put(key.toLowerCase(), value);
 	}
 	
+	@Override
+	public DatabaseRow put(String key, Object value) {
+		super.put(key, value);
+		return this;
+	}
+	
+	@Override
+	public DatabaseRow putAll(Map<String, Object> values) {
+		super.putAll(values);
+		return this;
+	}
+	
 	public Map<String, Object> getValues() {
 		return toMap();
 	}
