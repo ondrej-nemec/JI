3a242a7b7fee468c5394b57e89e8419d79cad6bb
Ondřej Němec <nikdynikde@gmail.com>
2023-10-05 22:09:16 +0200

    testcase with functional methods

--- ji-testing/src/main/java/ji/testing/TestCase.java
+++ ji-testing/src/main/java/ji/testing/TestCase.java
@@ -1,13 +1,26 @@
 package ji.testing;
 
-import ji.common.functions.Env;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import ji.common.structures.ThrowingFunction;
 
 public class TestCase {
-
-	protected final Env env;
 	
-	public TestCase(Env env) {
-		this.env = env;
+	public <T> Consumer<T> consumer(Class<T> clazz, Consumer<T> consumer) {
+		return consumer;
 	}
 
+	public <T> Supplier<T> supplier(Supplier<T> supplier) {
+		return supplier;
+	}
+
+	public <T, R> Function<T, R> function(Function<T, R> function) {
+		return function;
+	}
+
+	public <T, R, E extends Exception> ThrowingFunction<T, R, E> throwingFunction(ThrowingFunction<T, R, E> function) {
+		return function;
+	}
 }
