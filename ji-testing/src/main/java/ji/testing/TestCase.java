55e47201997bb05c0ff8791a012a427da6ed40b4
Ondřej Němec <nikdynikde@gmail.com>
2023-10-27 21:14:56 +0200

    test case is interface

--- ji-testing/src/main/java/ji/testing/TestCase.java
+++ ji-testing/src/main/java/ji/testing/TestCase.java
@@ -6,21 +6,21 @@ import java.util.function.Supplier;
 
 import ji.common.structures.ThrowingFunction;
 
-public class TestCase {
+public interface TestCase {
 	
-	public <T> Consumer<T> consumer(Class<T> clazz, Consumer<T> consumer) {
+	default <T> Consumer<T> consumer(Class<T> clazz, Consumer<T> consumer) {
 		return consumer;
 	}
 
-	public <T> Supplier<T> supplier(Supplier<T> supplier) {
+	default <T> Supplier<T> supplier(Supplier<T> supplier) {
 		return supplier;
 	}
 
-	public <T, R> Function<T, R> function(Function<T, R> function) {
+	default <T, R> Function<T, R> function(Function<T, R> function) {
 		return function;
 	}
 
-	public <T, R, E extends Exception> ThrowingFunction<T, R, E> throwingFunction(ThrowingFunction<T, R, E> function) {
+	default <T, R, E extends Exception> ThrowingFunction<T, R, E> throwingFunction(ThrowingFunction<T, R, E> function) {
 		return function;
 	}
 }
